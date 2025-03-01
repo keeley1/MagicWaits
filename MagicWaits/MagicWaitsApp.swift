5e5ef157faeff2dbb8ce91382ae1e58672119995
keeley1 <keeleygardner.gardner@gmail.com>
2025-03-01 23:20:17 +0000

    Initial Commit

---
+++ MagicWaits/MagicWaitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicWaitsApp.swift
+//  MagicWaits
+//
+//  Created by Keeley Gardner on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MagicWaitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MagicWaitsTests/MagicWaitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagicWaitsTests.swift
+//  MagicWaitsTests
+//
+//  Created by Keeley Gardner on 01/03/2025.
+//
+
+import Testing
+@testable import MagicWaits
+
+struct MagicWaitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
